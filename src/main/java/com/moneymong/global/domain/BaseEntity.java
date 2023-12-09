63ae91d6a4bb87759309979f73effd91a9c671ae
rlarltj123 <rlarltj1234@gmail.com>
2023-12-10 03:07:13 +0900

    feat: 생성/변경일자를 audit하는 BaseEntity를 추가한다.

---
+++ src/main/java/com/moneymong/global/domain/BaseEntity.java
@@ -0,0 +1,32 @@
+package com.moneymong.global.domain;
+
+import static lombok.AccessLevel.*;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    private boolean deleted;
+}
