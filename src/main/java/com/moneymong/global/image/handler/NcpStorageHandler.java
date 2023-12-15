f8e3ebbaa9e3955b4fb0893573376c8a6eae8af5
rlarltj123 <rlarltj1234@gmail.com>
2023-12-16 01:36:33 +0900

    feat: s3 인터페이스와 구현체 추가

---
+++ src/main/java/com/moneymong/global/image/handler/NcpStorageHandler.java
@@ -0,0 +1,63 @@
+package com.moneymong.global.image.handler;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.moneymong.global.image.dto.ImageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class NcpStorageHandler implements ImageStorageHandler{
+    private static final String ROOT_DIRNAME = "dev";
+
+    private final AmazonS3 amazonS3;
+
+    @Value("${cloud.ncp.s3.bucket}")
+    private String bucket;
+
+    @Override
+    public ImageResponse upload(File file, String dirName) {
+        String key = generateRandomFileName(file, dirName);
+        String path = putImage(file, key);
+        removeFile(file);
+
+        return new ImageResponse(key, path);
+    }
+
+    private void removeFile(File file) {
+        file.delete();
+    }
+
+    @Override
+    public void remove(String key) {
+        DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucket, key);
+        amazonS3.deleteObject(deleteObjectRequest);
+    }
+
+    @Override
+    public boolean doesObjectExists(String key) {
+        return amazonS3.doesObjectExist(bucket, key);
+    }
+
+    private String generateRandomFileName(File file, String dirName) {
+        return ROOT_DIRNAME + "/" + dirName + "/" + UUID.randomUUID().toString().substring(0, 8) + file.getName();
+    }
+
+    private String putImage(File uploadFile, String fileName) {
+        amazonS3.putObject(new PutObjectRequest(bucket, fileName, uploadFile)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+
+        return getImagePath(bucket, fileName);
+    }
+
+    private String getImagePath(String bucket, String fileName) {
+        return amazonS3.getUrl(bucket, fileName).toString();
+    }
+}
