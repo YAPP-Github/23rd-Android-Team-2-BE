f8127e08aaefcac87980d3240b6ef8a4a259626e
rlarltj123 <rlarltj1234@gmail.com>
2023-12-16 01:37:17 +0900

    feat: upload controller, service layer 추가

---
+++ src/main/java/com/moneymong/global/image/api/ImageController.java
@@ -0,0 +1,36 @@
+package com.moneymong.global.image.api;
+
+import com.moneymong.global.image.dto.ImageDeleteRequest;
+import com.moneymong.global.image.dto.ImageResponse;
+import com.moneymong.global.image.service.ImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+import static org.springframework.http.MediaType.*;
+import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/images")
+public class ImageController {
+
+    private final ImageService imageService;
+
+    @PostMapping(consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
+    public ImageResponse upload(@RequestPart("file") MultipartFile multipartFile, @ModelAttribute("dirName") String dirName) throws IOException {
+        return imageService.upload(multipartFile, dirName);
+    }
+
+    @DeleteMapping
+    public void remove(ImageDeleteRequest deleteRequest) {
+        imageService.remove(deleteRequest);
+    }
+}
---
+++ src/main/java/com/moneymong/global/image/service/ImageService.java
@@ -0,0 +1,50 @@
+package com.moneymong.global.image.service;
+
+import com.moneymong.global.exception.problem.ProblemParameters;
+import com.moneymong.global.image.dto.ImageDeleteRequest;
+import com.moneymong.global.image.exception.FileNotFoundProblem;
+import com.moneymong.global.image.exception.ImageNotExistsProblem;
+import com.moneymong.global.image.handler.ImageStorageHandler;
+import com.moneymong.global.image.dto.ImageResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ImageService {
+
+    private final ImageStorageHandler imageStorageHandler;
+
+    public ImageResponse upload(MultipartFile multipartFile, String dirName) throws IOException {
+        File file = convertMultipartFileToFile(multipartFile)
+                .orElseThrow(() -> new FileNotFoundProblem(ProblemParameters.of("file", multipartFile.getName())));
+
+        return imageStorageHandler.upload(file, dirName);
+    }
+
+    public void remove(ImageDeleteRequest deleteRequest) {
+        if (!imageStorageHandler.doesObjectExists(deleteRequest.getKey())) {
+            throw new ImageNotExistsProblem(ProblemParameters.of("key", deleteRequest.getKey()));
+        }
+
+        imageStorageHandler.remove(deleteRequest.getKey());
+    }
+
+    private Optional<File> convertMultipartFileToFile(MultipartFile multipartFile) throws IOException {
+        File file = new File(System.getProperty("user.dir") + "/" + multipartFile.getOriginalFilename());
+
+        if (file.createNewFile()) {
+            try (FileOutputStream fileOutputStream = new FileOutputStream(file)) {
+                fileOutputStream.write(multipartFile.getBytes());
+            }
+            return Optional.of(file);
+        }
+        return Optional.empty();
+    }
+}
