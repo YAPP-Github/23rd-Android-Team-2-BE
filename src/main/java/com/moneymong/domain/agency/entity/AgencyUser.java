13b35b4e4292a72e05c0c671ecf57d9b20a587c1
rlarltj123 <rlarltj1234@gmail.com>
2023-12-19 00:51:12 +0900

    feat: Agency Entity에 Builder 추가

--- src/main/java/com/moneymong/domain/agency/entity/AgencyUser.java
+++ src/main/java/com/moneymong/domain/agency/entity/AgencyUser.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.SQLDelete;
@@ -52,4 +53,12 @@ public class AgencyUser {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private AgencyUserRole agencyUserRole;
+
+    @Builder
+    private AgencyUser(Long id, Agency agency, String userToken, AgencyUserRole agencyUserRole) {
+        this.id = id;
+        this.agency = agency;
+        this.userToken = userToken;
+        this.agencyUserRole = agencyUserRole;
+    }
 }
