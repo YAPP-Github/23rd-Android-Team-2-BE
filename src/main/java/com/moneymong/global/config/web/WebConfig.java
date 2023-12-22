286f7d6db7d4c86e6baba4086eaff7075ffd8d2c
김기서 <81108344+rlarltj@users.noreply.github.com>
2023-12-23 00:56:10 +0900

    fix: cors 설정을 추가한다. (#21)
    * fix: cors 설정을 추가한다.
    
    * fix: OpenApiConfig 설정 추가

---
+++ src/main/java/com/moneymong/global/config/web/WebConfig.java
@@ -0,0 +1,21 @@
+package com.moneymong.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("/**")
+			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true);
+	}
+}
+
