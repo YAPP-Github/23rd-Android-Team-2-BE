6ff0f0a9108d780898ca4164d95cb619db278a47
rlarltj123 <rlarltj1234@gmail.com>
2023-12-25 14:26:19 +0900

    feat: User CRUD를 담당하는 Service Class 추가

--- src/main/java/com/moneymong/domain/user/repository/UserRepository.java
+++ src/main/java/com/moneymong/domain/user/repository/UserRepository.java
@@ -2,6 +2,23 @@ package com.moneymong.domain.user.repository;
 
 import com.moneymong.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+	//TODO QueryDSL로 refactor
+    @Query("""
+		SELECT u
+		FROM User u
+		WHERE u.provider = :provider
+		AND u.oauthId = :oauthId
+		""")
+    Optional<User> findByUserIdByProviderAndOauthId(
+            @Param("provider") String provider,
+            @Param("oauthId") String oauthId);
+
+    Optional<User> findByUserToken(String userToken);
 }
