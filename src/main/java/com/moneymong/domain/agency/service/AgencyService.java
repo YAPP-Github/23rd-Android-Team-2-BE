a7265c27176f7a3b0451863de11a67d51c327b57
rlarltj123 <rlarltj1234@gmail.com>
2024-01-04 22:54:08 +0900

    feat: Service Layer에 소속 생성 메소드 추가

--- src/main/java/com/moneymong/domain/agency/service/AgencyService.java
+++ src/main/java/com/moneymong/domain/agency/service/AgencyService.java
@@ -1,16 +1,23 @@
 package com.moneymong.domain.agency.service;
 
+import com.moneymong.domain.agency.api.request.CreateAgencyRequest;
+import com.moneymong.domain.agency.api.response.CreateAgencyResponse;
+import com.moneymong.domain.agency.entity.Agency;
 import com.moneymong.domain.agency.entity.AgencyUser;
+import com.moneymong.domain.agency.entity.enums.AgencyUserRole;
+import com.moneymong.domain.agency.repository.AgencyRepository;
 import com.moneymong.domain.agency.repository.AgencyUserRepository;
 import com.moneymong.global.exception.custom.NotFoundException;
 import com.moneymong.global.exception.enums.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class AgencyService {
     private final AgencyUserRepository agencyUserRepository;
+    private final AgencyRepository agencyRepository;
 
     public AgencyUser validateAgencyUser(
             final Long userId,
@@ -19,5 +26,23 @@ public class AgencyService {
         return agencyUserRepository
                 .findByUserIdAndAgencyId(userId, agencyId)
                 .orElseThrow(() -> new NotFoundException(ErrorCode.AGENCY_NOT_FOUND));
+    }
+
+    @Transactional
+    public CreateAgencyResponse create(Long userId, CreateAgencyRequest request) {
+        Agency agency = Agency.of(
+                request.getName(),
+                request.getAgencyType(),
+                request.getThumbnailImage(),
+                request.getDescription()
+        );
+
+        AgencyUser agencyUser = AgencyUser.of(agency, userId, AgencyUserRole.STAFF);
+
+        agency.addAgencyUser(agencyUser);
+
+        agencyRepository.save(agency);
+
+        return new CreateAgencyResponse(agency.getId());
     }
 }
