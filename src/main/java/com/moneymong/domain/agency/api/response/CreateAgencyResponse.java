a4d50b1f5de541d52992e3d23c8f426d84f50109
김기서 <81108344+rlarltj@users.noreply.github.com>
2024-01-05 00:35:29 +0900

    feat: 소속 생성 api를 추가한다. (#36)
    * feat: 소속에 소속 유저를 추가하는 메소드 생성
    
    * feat: Service Layer에 소속 생성 메소드 추가
    
    * feat: Agency 생성 api 추가
    
    * feat: headcount를 상수로 치환

---
+++ src/main/java/com/moneymong/domain/agency/api/response/CreateAgencyResponse.java
@@ -0,0 +1,10 @@
+package com.moneymong.domain.agency.api.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CreateAgencyResponse {
+    private Long id;
+}
