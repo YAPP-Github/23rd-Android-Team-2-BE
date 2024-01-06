c017b7de387bcbaa3153767ff48c0b073a0efd80
rlarltj123 <rlarltj1234@gmail.com>
2024-01-06 16:00:03 +0900

    chore: 대학교 검색 Tag 추가

--- src/main/java/com/moneymong/domain/university/api/UniversityController.java
+++ src/main/java/com/moneymong/domain/university/api/UniversityController.java
@@ -2,12 +2,14 @@ package com.moneymong.domain.university.api;
 
 import com.moneymong.domain.university.api.response.SearchUniversityResponse;
 import com.moneymong.domain.university.service.UniversityService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "2-1. [대학교 검색]")
 @RequestMapping("/api/v1/universities")
 @RequiredArgsConstructor
 @RestController
