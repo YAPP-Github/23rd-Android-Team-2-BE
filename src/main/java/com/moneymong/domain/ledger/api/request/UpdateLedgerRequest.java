fd2b6808d26bb231350007882d60a821d116854d
kimdoha <62235737+kimdoha@users.noreply.github.com>
2024-01-07 19:03:18 +0900

    refactor: MethodArgument 에러 응답 리팩토링 (#44)
    * feat: method-argument 응답 메시지 커스텀
    
    * chore: dto valid 수정

--- src/main/java/com/moneymong/domain/ledger/api/request/UpdateLedgerRequest.java
+++ src/main/java/com/moneymong/domain/ledger/api/request/UpdateLedgerRequest.java
@@ -2,6 +2,7 @@ package com.moneymong.domain.ledger.api.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import java.time.ZonedDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,16 +11,16 @@ import org.hibernate.validator.constraints.Length;
 @Getter
 @NoArgsConstructor
 public class UpdateLedgerRequest {
-    @NotBlank
-    @Length(min = 1, max = 20)
+    @NotBlank(message = "storeInfo를 입력해주세요.")
+    @Size(min = 1, max = 20, message = "storeInfo 1자 - 20자 입력해주세요.")
     private String storeInfo;
 
-    @NotNull
+    @NotNull(message = "amount를 입력해주세요.")
     private Integer amount;
 
-    @NotBlank
+    @NotBlank(message = "description를 입력해주세요.")
     private String description;
 
-    @NotNull
+    @NotNull(message = "paymentDate를 입력해주세요.")
     private ZonedDateTime paymentDate;
 }
--- src/main/java/com/moneymong/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/moneymong/global/exception/GlobalExceptionHandler.java
@@ -1,13 +1,19 @@
 package com.moneymong.global.exception;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moneymong.global.constant.MoneymongConstant;
 import com.moneymong.global.exception.custom.BusinessException;
 import com.moneymong.global.exception.dto.ErrorResponse;
+import com.moneymong.global.exception.dto.ErrorResponses;
 import com.moneymong.global.exception.enums.ErrorCode;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -27,18 +33,21 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
+    public ResponseEntity<ErrorResponses> handleMethodArgumentNotValidException(
             final MethodArgumentNotValidException exception
     ) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        List<String> messages = fieldErrors.stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .toList();
 
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(ErrorResponse.builder()
-                        .result(false)
-                        .status(HttpStatus.BAD_REQUEST.value())
-                        .message(exception.getMessage())
-                        .build()
-                );
+                .body(ErrorResponses.of(
+                        HttpStatus.BAD_REQUEST.value(),
+                        "GLOBAL-400",
+                        messages
+                ));
     }
 
     @ExceptionHandler(value = {Exception.class, RuntimeException.class, SQLException.class})
