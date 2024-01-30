a5450e98d7dab83fc46113cc22c702a15e840b50
김기서 <81108344+rlarltj@users.noreply.github.com>
2024-01-31 00:00:55 +0900

    setting: prometheus 설정을 추가한다. (#62)
    * setting: micrometer 의존성을 추가한다.
    
    * setting: actuator 설정 변경

--- src/main/java/com/moneymong/global/config/security/SecurityConfig.java
+++ src/main/java/com/moneymong/global/config/security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                     auth.requestMatchers("/favicon.ico").permitAll();
                     auth.requestMatchers("/api/v1/users").permitAll();
                     auth.requestMatchers("/api/v1/tokens").permitAll();
-                    auth.requestMatchers("/health").permitAll();
+                    auth.requestMatchers("/actuator/**").permitAll();
                     auth.anyRequest().authenticated();
                 })
                 .exceptionHandling(handler -> handler.authenticationEntryPoint(jwtAuthenticationEntryPoint))
