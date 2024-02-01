cee01d90562efde85fc3f593b8b3bf03e3f9f0f9
김기서 <81108344+rlarltj@users.noreply.github.com>
2024-02-01 21:36:23 +0900

    feat: logback 설정을 추가한다. (#63)
    * feat: logback 설정을 추가한다.
    
    * remove: test entity 및 미사용 import 제거

--- src/main/java/com/moneymong/domain/test/repository/TestRepository.java
+++
@@ -1,7 +0,0 @@
-package com.moneymong.domain.test.repository;
-
-import com.moneymong.domain.test.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestRepository extends JpaRepository<TestEntity, Long> {
-}
