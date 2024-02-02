9d09f11d181a34c4330e10e08f6e3c7e7255983c
rlarltj123 <rlarltj1234@gmail.com>
2024-02-02 16:06:17 +0900

    feat: 유저의 소속 가입 목록을 조회하는 메소드를 추가한다.

--- src/main/java/com/moneymong/domain/agency/repository/AgencyUserRepository.java
+++ src/main/java/com/moneymong/domain/agency/repository/AgencyUserRepository.java
@@ -1,9 +1,13 @@
 package com.moneymong.domain.agency.repository;
 
 import com.moneymong.domain.agency.entity.AgencyUser;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AgencyUserRepository extends JpaRepository<AgencyUser, Long>, AgencyUserRepositoryCustom {
-    Optional<AgencyUser> findByUserIdAndAgencyId(final Long userId, final Long agencyId);
+    Optional<AgencyUser> findByUserIdAndAgencyId(Long userId, Long agencyId);
+
+    List<AgencyUser> findAllByUserId(Long userId);
 }
