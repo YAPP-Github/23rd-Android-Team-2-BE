a254137284d2c4cdc08a00e3f0625572c7d3a8ed
김기서 <81108344+rlarltj@users.noreply.github.com>
2024-02-03 16:48:16 +0900

    chore: Exception handler에 logging을 추가한다. (#65)

--- src/main/java/com/moneymong/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/moneymong/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.moneymong.global.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.moneymong.global.constant.MoneymongConstant;
 import com.moneymong.global.exception.custom.BusinessException;
 import com.moneymong.global.exception.dto.ErrorResponse;
@@ -8,7 +7,6 @@ import com.moneymong.global.exception.dto.ErrorResponses;
 import com.moneymong.global.exception.enums.ErrorCode;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
@@ -21,6 +19,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String EXCEPTION_FORMAT = "[EXCEPTION]                   -----> ";
+    private static final String EXCEPTION_MESSAGE_FORMAT = "[EXCEPTION] EXCEPTION_MESSAGE -----> [{}]";
+    private static final String EXCEPTION_TYPE_FORMAT = "[EXCEPTION] EXCEPTION_TYPE    -----> [{}]";
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(
@@ -41,6 +43,8 @@ public class GlobalExceptionHandler {
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .toList();
 
+        logWarn(exception);
+
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(ErrorResponses.of(
@@ -54,9 +58,21 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleInternalException(
             final Exception exception
     ) {
-        log.info(exception.getMessage());
+        logError(exception);
+
         return ResponseEntity
                 .status(MoneymongConstant.INTERNAL_SERVER_ERROR)
                 .body(ErrorResponse.from(ErrorCode.INTERNAL_SERVER));
+    }
+
+    private void logError(Exception e) {
+        log.error(EXCEPTION_TYPE_FORMAT, e.getClass().getSimpleName());
+        log.error(EXCEPTION_MESSAGE_FORMAT, e.getMessage());
+        log.error(EXCEPTION_FORMAT, e);
+    }
+
+    private void logWarn(Exception e) {
+        log.warn(EXCEPTION_TYPE_FORMAT, e.getClass().getSimpleName());
+        log.warn(EXCEPTION_MESSAGE_FORMAT, e.getMessage());
     }
 }
