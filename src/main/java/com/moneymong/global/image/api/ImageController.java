4f9bc090ebcd8b0d8c41bbf74a68149cf8ffbf75
김기서 <81108344+rlarltj@users.noreply.github.com>
2024-05-02 02:10:17 +0900

    config: swagger scan 패키지를 수정한다. (#79)
    * feat: swagger scan 패키지를 수정한다.
    
    * feat: 누락된 Tag, Operation을 추가한다.

--- src/main/java/com/moneymong/global/image/api/ImageController.java
+++ src/main/java/com/moneymong/global/image/api/ImageController.java
@@ -3,6 +3,8 @@ package com.moneymong.global.image.api;
 import com.moneymong.global.image.dto.ImageDeleteRequest;
 import com.moneymong.global.image.dto.ImageResponse;
 import com.moneymong.global.image.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
+@Tag(name = "9. [이미지]")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/images")
@@ -23,12 +26,14 @@ public class ImageController {
 
     private final ImageService imageService;
 
+    @Operation(summary = "이미지 업로드")
     @PostMapping(consumes = {MULTIPART_FORM_DATA_VALUE, APPLICATION_JSON_VALUE})
     public ImageResponse upload(@RequestPart("file") MultipartFile multipartFile) {
         ImageResponse response = imageService.upload(multipartFile);
         return response;
     }
 
+    @Operation(summary = "이미지 삭제 API")
     @DeleteMapping(consumes = APPLICATION_JSON_VALUE)
     public void remove(@RequestBody ImageDeleteRequest deleteRequest) {
         imageService.remove(deleteRequest);
